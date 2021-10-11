b391e472829abfae70be440053dbc5b07065c6b7
gilangramadhahatno <gilangramadhanhatno@gmail.com>
2021-10-11 20:13:58 +0700

    Belajar React JS: Interaksi dengan backend, pemanggilan API(POST)

--- reactjs/src/container/BlogPost/BlogPost.jsx
+++ reactjs/src/container/BlogPost/BlogPost.jsx
@@ -9,20 +9,57 @@ import "./BlogPost.css";
 export default class BlogPost extends Component {
   state = {
     post: [],
+    formBlogPost: {
+      id: 1,
+      title: "",
+      body: "",
+      userId: 1,
+    },
   };
 
   getPostAPI = () => {
-    axios.get("http://localhost:3004/posts").then((res) => {
+    axios.get("http://localhost:3004/posts?_sort=id&_order=desc").then((res) => {
       this.setState({
         post: res.data,
       });
     });
   };
 
+  postDataToAPI = () => {
+    axios.post("http://localhost:3004/posts", this.state.formBlogPost).then(
+      (res) => {
+        console.log(res);
+        this.getPostAPI();
+      },
+      (err) => {
+        console.log("error", err);
+      }
+    );
+  };
+
   handleRemove = (data) => {
     axios.delete(`http://localhost:3004/posts/${data}`).then((res) => {
       this.getPostAPI();
     });
+  };
+
+  handleFormChange = (event) => {
+    let formBlogPostNew = { ...this.state.formBlogPost };
+    let timestamp = new Date().getTime();
+    formBlogPostNew["id"] = timestamp;
+    formBlogPostNew[event.target.name] = event.target.value;
+    this.setState(
+      {
+        formBlogPost: formBlogPostNew,
+      },
+      () => {
+        console.log("value obj formBlogPost: ", this.state.formBlogPost);
+      }
+    );
+  };
+
+  handleSubmit = () => {
+    this.postDataToAPI();
   };
 
   componentDidMount() {
@@ -57,6 +94,15 @@ export default class BlogPost extends Component {
     return (
       <>
         <p className="section-title">Blog Post</p>
+        <div className="form-add-post">
+          <label htmlFor="title">Title</label>
+          <input type="text" name="title" placeholder="add title" onChange={this.handleFormChange} />
+          <label htmlFor="body">Blog Content</label>
+          <textarea name="body" id="body" cols="30" rows="10" placeholder="add blog content" onChange={this.handleFormChange}></textarea>
+          <button className="btn-submit" onClick={this.handleSubmit}>
+            Simpan
+          </button>
+        </div>
         {this.state.post.map((post) => {
           return <Post key={post.id} data={post} remove={this.handleRemove} />;
         })}
