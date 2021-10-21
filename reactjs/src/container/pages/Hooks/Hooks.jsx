ebd733d0d451e848be0310153fe7068fe8aa663c
gilangramadhahatno <gilangramadhanhatno@gmail.com>
2021-10-21 18:46:08 +0700

    Belajar React JS: React Hooks - Side Effect/useEffect() atau LifeCycle

--- reactjs/src/container/pages/Hooks/Hooks.jsx
+++ reactjs/src/container/pages/Hooks/Hooks.jsx
@@ -1,9 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import "./Hooks.css";
 
 export default function Hooks() {
   const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    document.title = `Title Change : ${count}`;
+    return () => {
+      document.title = "React JS Hello World";
+    };
+  });
 
   return (
     <div className="p-hooks">
